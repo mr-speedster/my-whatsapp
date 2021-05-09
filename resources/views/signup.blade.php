c272bb2fbc4c3d843a180baecd80db51c4079037
Ajith <ajithd78564@gmail.com>
2021-05-09 21:45:14 +0530

    first commit

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -29,18 +29,36 @@
            		   </div>
            		   <div class="div">
            		   		<h5>Username</h5>
-           		   		<input type="text" name="user_name" class="input">
+           		   		<input type="text" name="user_name"  required class="input">
            		   </div>
            		</div>
+                   <div class="input-div one">
+                    <div class="i">
+                            <i class="fas fa-mail"></i>
+                    </div>
+                    <div class="div">
+                            <h5>email</h5>
+                            <input type="text" name="user_mail" required class="input">
+                    </div>
+                 </div>
            		<div class="input-div pass">
            		   <div class="i"> 
            		    	<i class="fas fa-lock"></i>
            		   </div>
            		   <div class="div">
            		    	<h5>Password</h5>
-           		    	<input type="password" name="user_pass" class="input">
+           		    	<input type="password" name="user_pass" required class="input">
             	   </div>
             	</div>
+                <div class="input-div pass">
+                    <div class="i"> 
+                         <i class="fas fa-lock"></i>
+                    </div>
+                    <div class="div">
+                         <h5>Conform</h5>
+                         <input type="password" name="conform_pass" required class="input">
+                 </div>
+              </div>
             	<a href="#">Forgot Password?</a>
             	<input type="submit" class="btn" value="Login">
             </form>
